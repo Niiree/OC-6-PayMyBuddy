f1f8701660f50a8f6b02b6e22c0438335d476dda
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2021-11-13 02:54:42 +0100

    update Service

--- src/main/java/com/paymybuddy/app/services/TransactionService.java
+++ src/main/java/com/paymybuddy/app/services/TransactionService.java
@@ -1,5 +1,7 @@
 package com.paymybuddy.app.services;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,23 +23,50 @@ public class TransactionService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	
+	public Iterable<Transaction> findAll(){
+		return transactionRepository.findAll();
+	}
+	
+	public Optional<Transaction> findById() {
+		
+		return transactionRepository.findById(1);
+	}
+	
 	@Transactional
-	public void createTransaction(Transaction transaction) {
-		 //User userRecipient = userRepository.findById(transaction.getId_user_recipient()).get();
-		// User userSend = userRepository.findById(transaction.getId_user_spend()).get();
-		 
-		// if(userRecipient.getStatut_active() && userSend.getStatut_active()) {
-		//	 if(userSend.getSolde() - transaction.getBalance() > 0) {
-	//			userSend.setSolde(userSend.getSolde() - transaction.getBalance()); 
-		//	 }
-			 
-			 
-	//	 }
-		 
-		 
+	public Transaction createTransaction(Transaction transaction) throws Exception {
+		User emitter = transaction.getEmitter();
+		User receiver = transaction.getReceiver();
+		
+		if(emitter != receiver) {
+			if(receiver.getStatut_active()) {
+				if(emitter.getBalance() - transaction.getBalance() > 0) {
+					emitter.setBalance(emitter.getBalance()-transaction.getBalance());
+					receiver.setBalance(receiver.getBalance()+transaction.getBalance());
+					transaction.setDate_transaction(LocalDate.now());
+					transaction.setEmitter(emitter);
+					transaction.setReceiver(receiver);
+					transaction.setStatut_transaction(true);
+					return transactionRepository.save(transaction);
+				
+				}else {
+					throw new Exception("Not enough money");
+				}	
+			}else {
+				throw new Exception ("User receiver was not active");
+			}
+		}else {
+			throw new Exception ("Same user");
+		}
+		
 		
 		
 	}
+		 
+		 
+		
+		
+	
 	
 
 
