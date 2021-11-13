af18270a9c920918fc87012bef38cc5f8969465c
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2021-11-13 02:53:23 +0100

    update controller

--- src/main/java/com/paymybuddy/app/controllers/TransactionController.java
+++ src/main/java/com/paymybuddy/app/controllers/TransactionController.java
@@ -1,5 +1,31 @@
 package com.paymybuddy.app.controllers;
 
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.paymybuddy.app.models.Transaction;
+import com.paymybuddy.app.services.TransactionService;
+
+@Controller
 public class TransactionController {
 
+	@Autowired
+	private TransactionService transactionService;
+	
+	
+	@RequestMapping("/transactions")
+	public String transaction(Model model) {
+		Iterable<Transaction> transactions = transactionService.findAll();
+		model.addAttribute("transactions",transactions);
+		return "transaction_history";
+	}
+	
+	
+	
 }
--- src/main/java/com/paymybuddy/app/controllers/UserController.java
+++ src/main/java/com/paymybuddy/app/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.paymybuddy.app.controllers;
 
 
 
-
 public class UserController {
 
 }
