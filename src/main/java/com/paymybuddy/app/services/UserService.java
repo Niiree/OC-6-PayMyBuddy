4d7165e0d4601341be1d86b96f7c37eff26af4ff
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2021-11-14 18:31:54 +0100

    Ajout sur SaveUser

--- src/main/java/com/paymybuddy/app/services/UserService.java
+++ src/main/java/com/paymybuddy/app/services/UserService.java
@@ -1,5 +1,7 @@
 package com.paymybuddy.app.services;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,15 @@ import com.paymybuddy.app.repository.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void saveUser(User user) {
+		user.setDate_creation(LocalDate.now());
+		//TODO vérification à faire email
+		userRepository.save(user);
+	}
 	
 	
 
