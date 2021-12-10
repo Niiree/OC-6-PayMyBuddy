0c5ca4e1a421bd8e4cfbea0c870e5eef31ea77a2
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2021-12-10 15:24:31 +0100

    Ajout AppController

---
+++ src/main/java/com/paymybuddy/app/controllers/AppController.java
@@ -0,0 +1,20 @@
+package com.paymybuddy.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.paymybuddy.app.repository.UserRepository;
+
+@Controller
+public class AppController {
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	@GetMapping("/essai")
+	public String viewHomePage() {
+		return "essai";
+	}
+
+}
