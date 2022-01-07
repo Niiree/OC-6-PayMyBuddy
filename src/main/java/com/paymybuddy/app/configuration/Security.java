3501b366dcbe00ff3704facba0d22e036baed664
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2022-01-07 20:59:37 +0100

    update de la sécurité du projet + login

--- src/main/java/com/paymybuddy/app/configuration/Security.java
+++ src/main/java/com/paymybuddy/app/configuration/Security.java
@@ -8,11 +8,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import com.paymybuddy.app.services.CustomUserDetailsService;
 
@@ -52,16 +54,24 @@ public class Security extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/users").authenticated()
-            .anyRequest().permitAll()
+            .antMatchers("/register")
+            .permitAll()
+            .anyRequest().authenticated()
             .and()
             .formLogin()
-                .usernameParameter("email")
-                .defaultSuccessUrl("/users")
-                .permitAll()
+               .usernameParameter("email")
+               .defaultSuccessUrl("/users")
+               .permitAll()
             .and()
             .logout().logoutSuccessUrl("/").permitAll();
     }
     
+    
+   /* @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**").anyRequest(); //Exclusion du dossier resources
+    }*/
+
 
 }
