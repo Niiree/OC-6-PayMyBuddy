2c330ef47c60f60ceed8bee2af30fab0f608a619
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2022-02-11 20:20:34 +0100

    LocalDate to LocalDateTime

--- src/main/java/com/paymybuddy/app/controllers/AccountBankController.java
+++ src/main/java/com/paymybuddy/app/controllers/AccountBankController.java
@@ -7,13 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.paymybuddy.app.models.AccountBank;
 import com.paymybuddy.app.services.AccountBankService;
 import com.paymybuddy.app.services.UserService;
